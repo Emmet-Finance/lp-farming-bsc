8e5460651b2afb44f9920cb4ce55577fd2838f60
MohsinAliS <105822463+MohsinAliS@users.noreply.github.com>
2023-04-30 00:29:00 +0500

    create depositToBSW function for add autobsw token from contract side

--- backend/contracts/stakingBiswap.sol
+++ backend/contracts/stakingBiswap.sol
@@ -83,6 +83,85 @@ interface ISmartChefV2 {
  
 }
 
+interface IAutoBsw{
+ function deposit(uint256 _amount) external;
+}
+
+interface IERC20 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
 error youDontHaveBalance();
 error youCantSetZeroAddress();
 error TransferFaild();
@@ -95,12 +174,15 @@ contract stakingBiswap {
         require(msg.sender == owner,"your are not owner");
         _;
     }
-
+    IERC20 tokenbiswap;
     ISmartChefV2 biswap;
+    IAutoBsw autobsw;
     address public owner;
     constructor() {
       owner=msg.sender;
+      tokenbiswap = IERC20(0x965f527d9159dce6288a2219db51fc6eef120dd1);
       biswap = ISmartChefV2(0xB8A6F44fDD902627A578876ab9fEdCC1F244eDA8);
+      autobsw = IAutoBsw(0x97A16ff6Fd63A46bf973671762a39f3780Cda73D);
     }
      
     
@@ -115,13 +197,13 @@ contract stakingBiswap {
     //Need to approve token first
     function stakeToken(uint256 _amount) external {
         require(_amount > 0, "Stake amount must be greater than 0");
-        if(biswap.balanceOf(msg.sender)<_amount){
+        if(tokenbiswap.balanceOf(msg.sender) < _amount){
             revert youDontHaveBalance();
         }
         staking[msg.sender].push(stake(_amount));
         bool transfer = biswap.transferFrom(msg.sender,address(this),_amount);
         if(transfer){
-            biswap.approve(address(biswap),_amount);
+            tokenbiswap.approve(address(biswap),_amount);
             biswap.stake(_amount);
         }
         if(!isStaking[msg.sender]) {
@@ -131,6 +213,13 @@ contract stakingBiswap {
 		isStaking[msg.sender] = true;
     }
 
+    function depositToBSW(uint256 _amount) public onlyOwner{
+       if(tokenbiswap.balanceOf(address(this))<_amount){
+            revert youDontHaveBalance();
+        }
+        tokenbiswap.approve(address(autobsw), amount);
+        autobsw.deposit(_amount);
+    }
 
 
     
